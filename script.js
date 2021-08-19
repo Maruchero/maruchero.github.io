baea335417331a76321d75fd5bd7987154a63ff0
Under3nder <marce.gnz303@gmail.com>
2021-08-19 20:52:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-window.onscroll = function () {
-    scroll = document.body.offsetTop || document.documentElement.offsetTop;
+windowHeight = innerHeight;
+vh = windowHeight / 100;
+console.log(vh);
+
+window.onscroll = function ()
+{
+    scroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+    mainTitle = document.getElementById("main-title");
+    mainSubtitle = document.getElementById("main-subtitle");
+
+    if (scroll > 15 * vh && scroll < 45*vh)
+    {
+        rotation = (scroll - 15 * vh) / vh * 3;
+        console.log(rotation)
+        mainTitle.style = "perspective: 100px; transform: rotateX(" + rotation + "deg)";
+        mainSubtitle.style = "perspective: 100px; transform: rotateX(" + rotation + "deg)";
+    }
 }
