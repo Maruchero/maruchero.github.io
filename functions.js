4d2bbb7668d082174c2766e4f3710fa1ee47d740
Under3nder <marce.gnz303@gmail.com>
2021-12-29 13:09:53 +0100

    added menu

--- functions.js
+++ functions.js
@@ -39,3 +39,31 @@ function openMenu() {
 function closeMenu() {
     menu.style = "transform: translateX(-100%); box-shadow: none;";
 }
+
+// Dropdown
+let content = document.getElementsByClassName("content");
+
+let tempDropdowns = document.getElementsByClassName("dropdown");
+let dropdowns = [];
+for (i=0; i<tempDropdowns.length; i++) {
+    // get span element to rotate it
+    dropdowns.push(tempDropdowns[i].lastChild);
+}
+
+function toggleDropdown(n) {
+    // calc content absolute height
+    let children = content[n].children;
+    let height = 0;
+    for (let i=0; i<children.length; i++) {
+        height += children[i].offsetHeight;
+    }
+
+    // toggle mode
+    if (content[n].style.maxHeight == 0) {
+        content[n].style.maxHeight = height + 'px';
+        dropdowns[n].style.transform = 'translate(-50%, -50%) rotate(135deg)';
+    } else {
+        content[n].style = '';
+        dropdowns[n].style = '';
+    }
+}
