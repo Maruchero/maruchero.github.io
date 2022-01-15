6b1ac8f85e4e11883266c661eeafc68af6dcaa9b
Under3nder <marce.gnz303@gmail.com>
2022-01-15 11:28:35 +0100

    Bugs fixed in menu and cards, changed navbar icons

---
+++ README.md
@@ -0,0 +1,27 @@
+# My website
+
+![Language](https://img.shields.io/badge/-HTML-red)
+![Language](https://img.shields.io/badge/-CSS-blue)
+![Language](https://img.shields.io/badge/-JS-yellow)
+![Language](https://img.shields.io/badge/Web-Developement-green)
+
+Hi, whit is my website repository, to see the result go on [under3nder.github.io](under3nder.github.io). I hope you like it.
+If you find some issues feel free to contact me at my [email](#contacts) down below
+
+## Content
+
+### Menu
+
+- Actually empty ;(
+
+### Home page
+ 
+- Navbar
+- Header
+- Programming languages section
+- Web languages section
+- Web skills section
+- Footer
+
+## Contacts
+marce.gnz303@gmail.com
--- fade-in.js
+++ fade-in.js
@@ -1,13 +1,17 @@
 /*
  * To use this script you just need to add the "fade-in" class
  * to the element you want to use the fade-in animation on.
- * 
+ *
  * It will inject a some style in the head, but don't worry,
  * it won't change anything. Enjoy the magic.
- * 
+ *
  * You can change the attributes down below to personalize your animation.
  */
 
+/***********************************
+ * PREDEFINED VALUES
+ * Do not modify!
+ */
 const vh = window.innerHeight / 100;
 
 /* ******************************************
@@ -17,39 +21,46 @@ const vh = window.innerHeight / 100;
 
 // time needed for completing the animation(must be string)
 // default is 0.5 seconds
-const time = ".3s"
+const time = ".5s";
 
 // decides at which distance from the top(in pixels) the element will animate
 // default is 80% of the screen height
-const animationAt = 80*vh
+const animationAt = 80 * vh;
+
+// this is the name of the class that fade-in animation will be applied on
+// default value is "fade-in"
+const affectedClass = "fade-in";
 
 // ******************************************
 
 // Create and inject needed style
 let style = document.createElement("style");
-style.innerHTML = "/*Injected from FADE-IN*/\n" + 
-".fade-in {\n" + 
-"    transform: translateY(50px);\n" + 
-"    filter: opacity(0);\n" + 
-"}\n\n" + 
-"@keyframes fadeIn {\n" + 
-"    0% {\n" + 
-"        transform: translateY(50px);\n" + 
-"        filter: opacity(0);\n" + 
-"    }\n" + 
-"    100% {\n" + 
-"        transform: translateY(0);\n" + 
-"        filter: opacity(1);\n" + 
-"    }\n" + 
-"}\n";
+style.innerHTML =
+    "/*Injected from FADE-IN*/\n" +
+    "." +
+    affectedClass +
+    " {\n" +
+    "    transform: translateY(50px);\n" +
+    "    filter: opacity(0);\n" +
+    "}\n\n" +
+    "@keyframes fadeIn {\n" +
+    "    0% {\n" +
+    "        transform: translateY(50px);\n" +
+    "        filter: opacity(0);\n" +
+    "    }\n" +
+    "    100% {\n" +
+    "        transform: translateY(0);\n" +
+    "        filter: opacity(1);\n" +
+    "    }\n" +
+    "}\n";
 document.head.appendChild(style);
 
 // Get fade-in elements
-const fadeIn = document.getElementsByClassName("fade-in");
+const fadeIn = document.getElementsByClassName(affectedClass);
 
 // Get offset top of elements
 const fadeOffsetTop = [];
-for (i=0; i<fadeIn.length; i++) {
+for (i = 0; i < fadeIn.length; i++) {
     fadeOffsetTop.push(fadeIn[i].offsetTop);
 }
 
@@ -59,23 +70,25 @@ window.onscroll = () => {
 
     // for each element check if it passed animationAt
     let k = 0;
-    for (i=0; i<fadeIn.length; i++) {
+    for (i = 0; i < fadeIn.length; i++) {
         let windowTopDistance = fadeOffsetTop[i] - yScroll;
 
         // if passed animationAt
         if (windowTopDistance < animationAt) {
             // search for elements at the same height(like cards)
-            for (j=0; j<i; j++) {
+            for (j = 0; j <= i; j++) {
                 if (fadeOffsetTop[j] == fadeOffsetTop[i]) {
-                    fadeIn[i].style.animation = "fadeIn " + time + " " + (++k * 300) + "ms ease forwards";
+                    fadeIn[i].style.animation =
+                        "fadeIn " +
+                        time +
+                        " " +
+                        k++ * 300 +
+                        "ms cubic-bezier(0.29, 0.73, 0.29, 0.7) forwards";
                     break;
                 } else {
                     k = 0;
                 }
             }
-            if (k == 0) {
-                fadeIn[i].style.animation = "fadeIn " + time + " ease forwards";
-            }
         }
     }
-}
+};
