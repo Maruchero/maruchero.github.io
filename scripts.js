d31bf795261c49f4f723cfd59a34e08c4980936e
Under3nder <marce.gnz303@gmail.com>
2022-09-02 22:08:40 +0200

    Fade in

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/**********************************************************************
+ * Fade-in
+ */
+const windowHeight = window.innerHeight;
+const activationPoint = windowHeight * 0.8;
+const fadeIn = Array.from(document.querySelectorAll(".fade-in"));
+
+window.onscroll = () => {
+  for (let i = 0; i < fadeIn.length; i++) {
+    if (fadeIn[i].getBoundingClientRect().top < activationPoint) {
+      fadeIn[i].classList.add("active");
+      fadeIn.splice(i, 1);
+    }
+  }
+};
