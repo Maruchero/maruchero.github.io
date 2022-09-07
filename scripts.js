16cb57fa0014dea39eb64a7c663610df89a3f8a4
Under3nder <marce.gnz303@gmail.com>
2022-09-07 22:39:43 +0200

    P4Password features

--- scripts.js
+++ scripts.js
@@ -5,11 +5,16 @@ const windowHeight = window.innerHeight;
 const activationPoint = windowHeight * 0.9;
 const fadeIn = Array.from(document.querySelectorAll(".fade-in"));
 
-window.onscroll = () => {
+function checkFadeIns() {
   for (let i = 0; i < fadeIn.length; i++) {
     if (fadeIn[i].getBoundingClientRect().top < activationPoint) {
       fadeIn[i].classList.add("active");
       fadeIn.splice(i, 1);
     }
   }
+}
+
+checkFadeIns();
+window.onscroll = () => {
+  checkFadeIns();
 };
